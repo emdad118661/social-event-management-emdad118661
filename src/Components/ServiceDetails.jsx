cc9219803793d41328846cf9710fc268565cdc91
emdad118661 <emdad118661@gmail.com>
2024-05-30 21:18:20 +0600

    asset

--- src/Components/ServiceDetails.jsx
+++ src/Components/ServiceDetails.jsx
@@ -1,7 +1,7 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ServiceDetails = () => {
     const services = useLoaderData();
@@ -11,8 +11,8 @@ const ServiceDetails = () => {
 
     const notify = () => toast("Booked successfully!!");
     return (
-        <div className="flex mx-auto max-w-7xl">
-            <div className="w-1/2">
+        <div className="flex flex-col mx-auto md:flex-row max-w-7xl">
+            <div className="order-2 md:order-1 md:w-1/2">
                 <h1 className="text-2xl"><b>Service:</b> {service.service_name}</h1>
                 <br />
                 <p><b>Description:</b> {service.big_description}</p>
@@ -22,11 +22,11 @@ const ServiceDetails = () => {
                 <button onClick={notify} className="btn btn-primary">Booking: {service.price}</button>
                 <ToastContainer />
             </div>
-            <div className="mt-10 ml-10">
+            <div className="order-1 mt-10 md:order-2 md:ml-10">
                 <img src={service.image_details_url} alt="" />
             </div>
-            
         </div>
+
     );
 };
 
